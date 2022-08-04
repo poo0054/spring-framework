059b9e200e3e272e2c905380fbdb21fe3baedf0a
zhangzhi <poo0054@outlook.com>
2022-08-04 11:33:59 +0800

    spring监听器 - 注解

--- spring-study/src/main/java/com/poo0054/study/listener/AnnotationListenerDemo.java
+++ spring-study/src/main/java/com/poo0054/study/listener/AnnotationListenerDemo.java
@@ -1,10 +1,10 @@
 package com.poo0054.study.listener;
 
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.context.config.ContextNamespaceHandler;
 import org.springframework.context.event.EventListener;
-import org.w3c.dom.Element;
+import org.springframework.context.event.EventListenerFactory;
+import org.springframework.context.event.EventListenerMethodProcessor;
 
 /**
  * @author ZhangZhi
@@ -16,13 +16,10 @@ public class AnnotationListenerDemo {
 	/**
 	 * 在spring中，需要注入{@link org.springframework.context.event.EventListenerMethodProcessor}和{@link org.springframework.context.event.DefaultEventListenerFactory}
 	 * <p>
-	 * 这俩个类 如果添加<context:annotation-config/>作用也是一样的
-	 * 原理：
-	 * ---> {@link ContextNamespaceHandler#init()}方法中初始化 AnnotationConfigBeanDefinitionParser
-	 * {@link org.springframework.context.annotation.AnnotationConfigBeanDefinitionParser#parse(Element, ParserContext)}中调用了{@link org.springframework.context.annotation.AnnotationConfigUtils#registerAnnotationConfigProcessors(BeanDefinitionRegistry)}
-	 * 方法，里面注入了{@link org.springframework.context.event.EventListenerMethodProcessor}和{@link org.springframework.context.event.DefaultEventListenerFactory}俩个类
-	 * <p>
-	 * 在springboot中，也是这样使用的，在创建createApplicationContext中调用{@link org.springframework.context.annotation.AnnotationConfigUtils#registerAnnotationConfigProcessors(BeanDefinitionRegistry)}
+	 * 这俩个类 如果添加<context:annotation-config/>作用也是一样的，因为：
+	 * {@link ContextNamespaceHandler#init()}方法中初始化 {@link org.springframework.context.annotation.AnnotationConfigBeanDefinitionParser}
+	 * {@link org.springframework.context.event.EventListenerMethodProcessor#postProcessBeanFactory(ConfigurableListableBeanFactory)}中添加{@link EventListenerFactory}找到 DefaultEventListenerFactory，
+	 * 在{@link EventListenerMethodProcessor#afterSingletonsInstantiated()}中使用DefaultEventListenerFactory创建加了注解的方法并加入（addApplicationListener）多播监听器
 	 *
 	 * @param event 事件
 	 */
