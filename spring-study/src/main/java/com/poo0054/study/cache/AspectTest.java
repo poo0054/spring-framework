3ae2bd2775299ffee33172ef5b9369643ae480e4
poo0054 <poo0054@outlook.com>
2023-03-06 11:52:26 +0800

    cache 和 aop  事务都是使用AnnotationAwareAspectJAutoProxyCreator  其中子类有优先级 其中一个生效:一般:  AnnotationAwareAspectJAutoProxyCreator

---
+++ spring-study/src/main/java/com/poo0054/study/cache/AspectTest.java
@@ -0,0 +1,27 @@
+package com.poo0054.study.cache;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+/**
+ * @author ZhangZhi
+ * @version 1.0
+ * @since 2022/6/28 17:47
+ */
+@Aspect
+public class AspectTest {
+
+	@Pointcut("execution(* com.poo0054.study.cache.CacheTest.*(..))")
+	private void pointCut() {
+	}
+
+	@Around("pointCut()")
+	public Object touchBean(ProceedingJoinPoint joinPoint) throws Throwable {
+		System.out.println("I is  Around before");
+		Object proceed = joinPoint.proceed();
+		System.out.println("I is Around after");
+		return proceed;
+	}
+}
